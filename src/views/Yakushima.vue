0d04b91cdb19d95c236772d25fb108c36d074b53
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-05-06 15:49:41 +0900

    yakushima

--- src/views/Yakushima.vue
+++ src/views/Yakushima.vue
@@ -10,28 +10,49 @@
           千尋の滝
         </h4>
         <p class="content__about item">
-          
+          壮大なＶ字谷の中央から水が流れ落ちる落差60メートルの滝。<br>
+          山肌が崩れないようにセメントで固めているようにも見える滝の左側にある岩盤は、<br>
+          実は大きな一枚の岩でできており、まるで千人が手を結んだくらいの大きさということから<br>
+          名づけられた、屋久島の特徴を実感する景観です。<br>
+          その迫力に驚きます。
         </p>
       </div>
     </section>
     <section class="sec">
-      <h2 class="sec__title">タイ・アユタヤ</h2>
+      <h2 class="sec__title">屋久島</h2>
       <div class="content appear up">
         <div class="content__img item">
-          <img src="../assets/images/IMG_0785.png" alt="">
+          <img src="../assets/images/IMG_7007.png" alt="">
         </div>
         <h4 class="content__name item">
-          ワット・チャイワタナラーム
+          縄文杉
         </h4>
         <p class="content__about item">
-          アユタヤには魅力的な遺跡がたくさんあります。<br>
-          バンコクから電車で約90分かけて訪れる価値は大いにあります。<br>
-          古都として、街全体が世界遺産でありバンコクとはまた違った魅力で溢れています。<br>
-          一度訪れるとリピートしたくなること間違いなしです。<br>
-          私はレンタサイクルで街をめぐりました。
+          屋久島の定番、そしてシンボルとも言える縄文杉。現在確認されている中で最大の屋久杉。<br>
+          杉までのその道程は簡単なものではありませんが、たどり着いたときのその感動はとても<br>
+          素晴らしいものです。見るものを圧倒するその姿に、見に来てよかったと心底思いました。<br>
+          屋久島を訪れた際には一見の価値アリです。
         </p>
       </div>
     </section>
+        <section class="sec">
+      <h2 class="sec__title">屋久島</h2>
+      <div class="content appear up">
+        <div class="content__img item">
+          <img src="../assets/images/IMG_5912.png" alt="">
+        </div>
+        <h4 class="content__name item">
+          太鼓岩・白谷雲水峡
+        </h4>
+        <p class="content__about item">
+          「もののけ姫」でシシ神の森のイメージとなった場所の存在する苔むす森はこの<br>
+          白谷雲水峡の道中にあります。片道2~3時間とこちらも険しい道なりますが、<br>
+          その景色にたどり着いたときの感動に勝るものはありません。<br>
+          雨の多い島ですが、晴れている日の絶景は本当に気持ちの良いものです！
+        </p>
+      </div>
+    </section>
+
   </div>
 </template>
 
