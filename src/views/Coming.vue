477889b303f7475d4b9a831cd2c5086890bfd02e
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-05-13 16:26:43 +0900

    strAnimate

--- src/views/Coming.vue
+++ src/views/Coming.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="content">
-    <section class="sec">
-      <h3 class="coming">Coming Soon</h3>
+    <section class="sec anoter">
+      <div class="animate">
+        <span class="char">C</span>
+        <span class="char">o</span>
+        <span class="char">m</span>
+        <span class="char">i</span>
+        <span class="char">n</span>
+        <span class="char">g</span>
+        <span class="char">S</span>
+        <span class="char">o</span>
+        <span class="char">o</span>
+        <span class="char">n</span>
+      </div>
     </section>
   </div>
 </template>
@@ -15,13 +26,21 @@ export default {
     return {
       title: 'Welcome To MyPage'
     }
+  },
+  methods: {
+    strAnimate() {
+      document.querySelector('.animate').classList.toggle('dancing')
+    }
+  },
+  mounted() {
+    this.strAnimate()
   }
 }
 </script>
 
 <style scoped>
 @import "../assets/styles/home.css";
-
+@import "../assets/styles/coming.css";
 
 </style>
 
